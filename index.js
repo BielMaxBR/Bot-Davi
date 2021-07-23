b8f402c6e0f2e43af527c5da6fc61623ea0a95b1
Gabriel Maximo <gbielmaximo@gmail.com>
2021-07-23 20:32:29 +0000

    clima

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const Discord = require("discord.js");
+const fetch = require('node-fetch')
+
 require('dotenv').config();
 
 const client = new Discord.Client();
@@ -9,7 +11,7 @@ client.on("ready", () => {
     console.log("Estou pronto para ser usado");
 })
 
-client.on("message", message => {
+client.on("message", async message => {
     const content = message.content
 
     if (content == `<@${client.user.id}>` && !message.author.bot) {
@@ -32,14 +34,57 @@ client.on("message", message => {
             message.reply("Estamos em desenvolvimento!")
             break;
         case "convite":
-            const embed = new Discord.MessageEmbed()
+            var embed = new Discord.MessageEmbed()
                 .setColor('#0099ff')
                 .setTitle('clique aqui para convidar esse bot')
                 .setURL('https://discord.com/oauth2/authorize?client_id=867903185988485141&scope=client&permissions=2048')
 
             message.reply(embed)
             break;
+        case "clima":
+            const url = "http://api.openweathermap.org/data/2.5/weather?"
 
+            const apiKey = "&appid=" + process.env.API
+            const lang = "&lang=pt_br"
+            const unit = "&units=metric"
+
+            let argsJunto = ""
+
+            args.forEach(arg => argsJunto += encodeURI(arg.toLowerCase()) + "+")
+
+            argsJunto = argsJunto.slice(0, argsJunto.length - 1)
+
+            const city = "&q=" + argsJunto
+            console.log(city)
+
+            const request = await fetch(url + city + apiKey + lang + unit)
+            const json = await request.json()
+
+            console.log(json)
+
+            if (json.cod == 404) {
+                message.channel.send("cidade não encontrada")
+                return
+            }
+
+            var embed = new Discord.MessageEmbed()
+                .setColor('#0099ff')
+                .setTitle(`Previsão do tempo para ${json.name} = ${json.sys.country}`)
+                .setDescription(`${json.weather[0].description}`)
+                .addFields(
+                    {
+                        name: "**Temperatura**", value: `Atual: ${json.main.temp} ºC \n
+                        Máxima: ${json.main.temp_max} ºC \n 
+                        Mínima: ${json.main.temp_min} ºC \n 
+                        Sensação térmica: ${json.main.feels_like} ºC`, inline: true
+                    },
+                    { name: "**Umidade**", value: `${json.main.humidity}%`, inline: true },
+                    { name: "**Velocidade do vento**", value: `${json.wind.speed} km/h`, inline: true },
+                    { name: "**Pressão do ar**", value: `${json.main.pressure} kPA`, inline: true }
+                )
+
+            message.channel.send(embed)
+            break;
     }
 
     /*
